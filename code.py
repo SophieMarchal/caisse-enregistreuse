1c2df8046a246e50249244c00032166008bb4270
SophieMarchal <65012062+SophieMarchal@users.noreply.github.com>
2020-05-27 17:46:48 +0200

    meilleur affichage et les cartes
    meilleur affichage : utilise displayio
    gère les cartes : certains codebarres representent des clients

--- code.py
+++ code.py
@@ -2,7 +2,11 @@ import time
 import board
 import json
 import busio
+import displayio
 import digitalio
+import terminalio
+from adafruit_display_text import label
+from adafruit_bitmap_font  import bitmap_font
 from adafruit_featherwing import minitft_featherwing
 
 wing = minitft_featherwing.MiniTFTFeatherWing()
@@ -11,7 +15,9 @@ uart = busio.UART(board.TX, board.RX, baudrate=9600)
 
 trg = digitalio.DigitalInOut(board.D13)
 trg.direction = digitalio.Direction.OUTPUT
-trg.value = False
+
+noto18 = bitmap_font.load_font('/fonts/Noto-18.bdf')
+noto18.load_glyphs('1234567890: '.encode('utf-8'))
 
 def scan():
   try:
@@ -21,7 +27,7 @@ def scan():
     trg.value = False
     time.sleep(0.2)
 
-    data = uart.read()
+    data = uart.readline()
     if data is not None:
       return ''.join(filter(lambda c: c > ' ', data.decode('latin1')))
     else:
@@ -29,8 +35,25 @@ def scan():
   finally:
     trg.value = False
 
+def ecriture(total,prix,nouveau_client, code_barre):
+    groupe= displayio.Group()
+    yabel=label.Label(noto18,text='total: {}'.format(total), x=20, y=40)
+    groupe.append(yabel)
+    if prix == None:
+        mabel=label.Label(terminalio.FONT, text=str(code_barre), x=20, y=20)
+    else:
+        mabel=label.Label(terminalio.FONT, text='prix= {}'.format(prix), x=20, y=20)
+    groupe.append(mabel)
+    if nouveau_client:
+        tabel=label.Label(terminalio.FONT,text= 'AU REVOIR', color= 0xff0000, x=70, y=70)
+        groupe.append(tabel)
+    wing.display.show(groupe)
+
 with open("prix.json", "r") as lecture_fichier:
     produits = json.load(lecture_fichier)
+
+with open("carte.json","r") as lecture_fichier:
+    carte = json.load(lecture_fichier)
 
 total=0
 
@@ -41,12 +64,15 @@ while True:
         code_barre = scan()
         if code_barre != None:
             if code_barre in produits:
-                total +=  produits[code_barre]
-                print(total)
-            else:
-                # print(code_barre)
-                print(total)
-                time.sleep(5)
-                print("NOUVEAU CLIENT")
+                prix= produits[code_barre]
+                total +=  prix
+                ecriture(total, prix, False, code_barre)
+            elif code_barre in carte:
+                ecriture(total,None,True, code_barre)
                 total=0
-        # else: pas de code barre, on retourne au début de la boucle
+            else:
+                print ("code-barre non connu")
+                ecriture(total,None,False, code_barre)
+        else:
+            ecriture(total,None, False, code_barre)
+    time.sleep(0.2)
